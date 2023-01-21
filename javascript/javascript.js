f770fac9e91993b21e5fd6d514d61701f3f85f71
santiparra <122759810+Santiparra@users.noreply.github.com>
2023-01-21 17:53:31 -0300

    Añadida funcion de tanteador. Añadida funcion para el turno humano.

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,46 +1,53 @@
 //Llevar un puntaje (del 0 al 4), si alguno llega a 2 el juego se termina
+
 let laIAEligio;
+let tuPuntaje = (0);
+let puntajeIA = (0);
 
     //Jugar una ronda individual
 
         //Función para randomizar la eleccion de la IA y Convertir los numeros del 0 al 2 a strings
 
-function randomIA() {
-    let randomNumero = Math.floor(Math.random() * 3);
-    if (randomNumero === 0) {randomNumero = "piedra";
-    } else if (randomNumero === 1) {randomNumero = "papel";
-    } else {randomNumero = "tijera"}
-    return randomNumero;
-}    
+    function randomIA() {
+        let randomNumero = Math.floor(Math.random() * 3);
+        if (randomNumero === 0) {randomNumero = "piedra";
+        } else if (randomNumero === 1) {randomNumero = "papel";
+        } else {randomNumero = "tijera"}
+        return randomNumero;
+    }    
 
         //Eleccion del jugador Humano
-        
-let cuadroDeEleccionHumano = prompt("Piedra, Papel o Tijera?")
-    
-
-let elHumanoEligio = cuadroDeEleccionHumano.toLowerCase();
-  
+    function turnoHumano() {        
+        let cuadroDeEleccionHumano = prompt("Piedra, Papel o Tijera?");
+        let elHumanoEligio = cuadroDeEleccionHumano.toLowerCase();
+        return elHumanoEligio;
+}  
             //Funcion para comparar ambos valores y definir ganador de la ronda
-    
-    
+   
 let resultadoRonda;
     
-function juegaUnaRonda(elHumanoEligio, laIAEligio) {
-    laIAEligio = randomIA();
-    if (laIAEligio === elHumanoEligio){
-         resultadoRonda = "Fue un empate"
-    } else if (elHumanoEligio === "piedra" && laIAEligio === "tijera" ||
-        elHumanoEligio === "papel" && laIAEligio === "piedra" ||
-        elHumanoEligio === "tijera" && laIAEligio === "papel") {
-            resultadoRonda = `Ganaste! ${elHumanoEligio} le gana a ${laIAEligio}`
-    } else {
-        resultadoRonda = `Perdiste! ${laIAEligio} le gana a ${elHumanoEligio}`
-}
-return resultadoRonda;
-}    
-
-
-    
+    function juegaUnaRonda(elHumanoEligio, laIAEligio) {
+        laIAEligio = randomIA();
+        if (laIAEligio === elHumanoEligio){
+            resultadoRonda = "Fue un empate"
+        } else if (elHumanoEligio === "piedra" && laIAEligio === "tijera" ||
+            elHumanoEligio === "papel" && laIAEligio === "piedra" ||
+            elHumanoEligio === "tijera" && laIAEligio === "papel") {
+                tuPuntaje = ++tuPuntaje;
+                resultadoRonda = `Ganaste! ${elHumanoEligio} le gana a ${laIAEligio}`
+        } else {
+            puntajeIA = ++puntajeIA;    
+            resultadoRonda = `Perdiste! ${laIAEligio} le gana a ${elHumanoEligio}`
+    }
+    return resultadoRonda + tuPuntaje + puntajeIA;
+    }       
     //Recalcular los puntajes
 
-console.log(juegaUnaRonda(elHumanoEligio, laIAEligio))
+function juego () {    
+    for (; tuPuntaje <5 && puntajeIA <5;) {
+        juegaUnaRonda(turnoHumano(), randomIA());
+        console.log(resultadoRonda + tuPuntaje + puntajeIA)
+    }
+}
+
+juego()
